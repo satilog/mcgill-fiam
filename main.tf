b14daf5d754a7e0cffffcddd41698445285b8c31
Sathyajit Loganathan <loganathansathyajit@gmail.com>
2024-09-25 14:10:21 -0600

    Add some experiments

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project = "mcgill-fiam-154324"
-  region  = "us-central1"  # Main region; you can change this as needed
+  region  = "us-central1"
 }
 
 # Define a firewall rule to allow access from any IP address to port 8888
@@ -19,23 +19,23 @@ resource "google_compute_firewall" "allow_jupyter" {
 # List of zones with available NVIDIA T4 GPUs to try
 variable "zones" {
   default = [
-    "us-central1-a",
-    "us-central1-b",
-    "us-central1-c",
-    "us-west1-a",
+    # "us-central1-a",
+    # "us-central1-b",
+    # "us-central1-c",
+    # "us-west1-a",
     "us-west1-b",
-    "us-west1-c",
-    "europe-west1-b",
-    "asia-east1-a"
+    # "us-west1-c",
+    # "europe-west1-b",
+    # "asia-east1-a"
   ]
 }
 
 # Create the VM instance, iterating over the list of zones
 resource "google_compute_instance" "training_vm" {
-  count        = length(var.zones)
-  name         = "training-vm-instance-${count.index}"
+  for_each = toset(var.zones)
+  name     = "training-vm-instance"
+  zone     = each.key
   machine_type = "n1-standard-4"
-  zone         = element(var.zones, count.index)
 
   guest_accelerator {
     type  = "nvidia-tesla-t4"
@@ -71,18 +71,15 @@ resource "google_compute_instance" "training_vm" {
     docker run --gpus all -it -d -p 8888:8888 --name pytorch-jupyter pytorch/pytorch:2.0.1-cuda11.7-cudnn8-runtime
   EOT
 
-  lifecycle {
-    ignore_changes = [count]
-  }
-
+  # Optional provisioner to log which instance was created
   provisioner "local-exec" {
-    command = "echo ${self.name} created in ${self.zone}"
-    when    = "create"
+    command    = "echo ${self.name} created in ${self.zone}"
+    when       = create
     on_failure = continue
   }
 }
 
-# Output the selected zone where the VM was successfully created
+# Output the zone of the first created instance
 output "selected_zone" {
-  value = google_compute_instance.training_vm[*].zone
+  value = values(google_compute_instance.training_vm)[0].zone
 }
